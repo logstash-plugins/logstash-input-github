15e0285f32515f7cc3755aad3b743b57fca591fe
Philippe Weber <philippeweber57@yahoo.fr>
2016-06-06 21:18:02 +0200

    update to new event API

--- lib/logstash/inputs/github.rb
+++ lib/logstash/inputs/github.rb
@@ -17,9 +17,9 @@ class LogStash::Inputs::GitHub < LogStash::Inputs::Base
   # Your GitHub Secret Token for the webhook
   config :secret_token, :validate => :string, :required => false
 
-  # If Secret is defined, we drop the events that don't match. 
-  # Otherwise, we'll just add a invalid tag
-  config :drop_invalid, :validate => :boolean
+  # If Secret is defined, we drop the events that don't match.
+  # Otherwise, we'll just add an invalid tag
+  config :drop_invalid, :validate => :boolean, :default => false
 
   def register
     require "ftw"
@@ -29,33 +29,44 @@ class LogStash::Inputs::GitHub < LogStash::Inputs::Base
   def run(output_queue)
     @server = FTW::WebServer.new(@ip, @port) do |request, response|
         body = request.read_body
-        begin
-          event = LogStash::Event.new(JSON.parse(body))
-        rescue JSON::ParserError => e
-          @logger.info("JSON parse failure. Falling back to plain-text", :error => e, :data => body)
-          event = LogStash::Event.new("message" => body, "tags" => "_invalidjson")
-        end
-        event['headers'] = request.headers.to_hash
-        if defined? @secret_token and event['headers']['x-hub-signature']
-            event['hash'] = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), @secret_token, body)
-            if not Rack::Utils.secure_compare(event['hash'], event['headers']['x-hub-signature'])
-                if not @drop_invalid
-                    event['tags'] = "_Invalid_Github_Message"
-                else
-                    @logger.info("Dropping invalid Github message")
-                    drop = true
-                end
-            end
-        end
-        if not drop
-            decorate(event)
-            output_queue << event
+        event = build_event_from_request(body, request.headers_.to_hash)
+        valid_event = verify_signature(event,body)
+        if !valid_event && @drop_invalid
+          @logger.info("Dropping invalid Github message")
+        else
+          decorate(event)
+          output_queue << event
         end
         response.status = 200
         response.body = "Accepted!"
     end
     @server.run
   end # def run
+
+  def build_event_from_request(body, headers)
+    begin
+      event = LogStash::Event.new(JSON.parse(body))
+    rescue JSON::ParserError => e
+      @logger.info("JSON parse failure. Falling back to plain-text", :error => e, :data => body)
+      event = LogStash::Event.new("message" => body, "tags" => "_invalidjson")
+    end
+    event.set('headers', headers)
+    return event
+  end
+
+  def verify_signature(event,body)
+    is_valid = true
+    sign_header = event.get("[headers][x-hub-signature]")
+    if @secret_token && sign_header
+      hash = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), @secret_token, body)
+      event.set("hash", hash)
+      if not Rack::Utils.secure_compare(hash, sign_header)
+        event.tag("_Invalid_Github_Message")
+        is_valid = false
+      end
+    end
+    return is_valid
+  end
 
   def close
     @server.stop
--- spec/inputs/github_spec.rb
+++ spec/inputs/github_spec.rb
@@ -9,4 +9,50 @@ describe  LogStash::Inputs::GitHub do
   it "register without errors" do
     expect { plugin.register }.to_not raise_error
   end
+
+  describe "building Logstash event from webhook" do
+    let(:body) {IO.read("spec/fixtures/event_create.json")}
+    let(:headers) { {"fake_header" => "fake_value"} }
+    let(:event) {plugin.build_event_from_request(body,headers)}
+
+    it "initialize event from webhook body" do
+      JSON.parse(body).each do |k,v|
+        expect(event.get(k)).to eq(v)
+      end
+    end
+
+    it "copy webhook http headers to event[headers]" do
+      expect(event.get('headers')).to eq (headers)
+    end
+  end
+
+  describe "verify webhook signature" do
+    let(:plugin) { LogStash::Plugin.lookup("input", "github").new( {"port" => 9999, "secret_token" => "my_secret"} ) }
+    let(:body) {IO.read("spec/fixtures/event_create.json")}
+    let(:headers) { {"x-hub-signature" => "hash"} }
+    let(:event) {plugin.build_event_from_request(body,headers)}
+    let(:hash) { "sha1=43b113fc453c47f1cd4d5b4ded2985581c00a715" }
+
+    it "accept event without signature" do
+      event.set('headers',{})
+      expect(plugin.verify_signature(event,body)).to eq(true)
+      expect(event.get("hash")).to be_nil
+      expect(event.get("tags")).to be_nil
+    end
+
+    it "reject event with invalid signature" do
+      event.set('headers',{"x-hub-signature" => "invalid"})
+      expect(plugin.verify_signature(event,body)).to eq(false)
+      expect(event.get("hash")).to eq(hash)
+      expect(event.get("tags")).to eq(["_Invalid_Github_Message"])
+    end
+
+    it "accept event with valid signature" do
+      event.set('headers', {"x-hub-signature" => hash})
+      expect(plugin.verify_signature(event,body)).to eq(true)
+      expect(event.get("hash")).to eq(hash)
+      expect(event.get("tags")).to be_nil
+    end
+
+  end
 end
